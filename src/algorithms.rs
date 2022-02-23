3c4fba37636ddafb83af88c5341f43eb68e9681b
banditpig <mike_k_houghton@yahoo.co.uk>
2022-02-23 13:02:41 +0000

    Refactor.

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -13,7 +13,7 @@ const SPRING_LENGTH: Scalar = 5.0;
 const DELTA_T: Scalar = 0.1;
 
 //Fattr(u, v) = Spring(u,v) - any repel force between u and v if v is a neighbour of u
-pub fn repel_force(pos_u: &Vect, pos_v: &Vect) -> Vect {
+pub fn repelling_force(pos_u: &Vect, pos_v: &Vect) -> Vect {
     //applies to node u and ALL other nodes
     let unit_uv = (*pos_v - *pos_u).as_unit_vector();
     let euc_dist = pos_u.euclid_distance(pos_v);
@@ -37,49 +37,77 @@ pub fn layout<T, E, ID: Debug + Copy + Ord + Clone + Hash + Eq>(
 
     println!("{:?}", positions);
     for _i in 1..100 {
-        let mut repel_forces: HashMap<ID, Vect> = HashMap::new();
-        for node_u in g.nodes.keys().sorted() {
-            let u = positions.get(node_u).unwrap();
-            let mut repel_u = Vect::new(0., 0., 0.);
-            for node_v in g.nodes.keys().sorted() {
-                if node_u != node_v {
-                    let v = positions.get(node_v).unwrap();
-                    repel_u = repel_u + repel_force(u, v);
-                }
-            }
-            repel_forces.insert(*node_u, repel_u);
-        }
-
-        let mut spring_forces: HashMap<ID, Vect> = HashMap::new();
-        for node_u in g.nodes.keys().sorted() {
-            let mut spring_u = Vect::new(0., 0., 0.);
-            let u = positions.get(node_u).unwrap();
-            for node_v in g.neighbors(*node_u).unwrap() {
-                let v = positions.get(&node_v).unwrap();
-                spring_u = spring_u + spring_force(u, v) - repel_force(u, v)
-            }
-            spring_forces.insert(*node_u, spring_u);
-        }
-
-        let mut resultant_forces: HashMap<ID, Vect> = HashMap::new();
-        for u in repel_forces.keys() {
-            let temp_repel = repel_forces.get(u).unwrap();
-            let temp_spring = spring_forces.get(u).unwrap();
-            resultant_forces.insert(*u, *temp_repel + *temp_spring);
-        }
-
-        //update positions
-        let mut new_positions: HashMap<ID, Vect> = HashMap::new();
-        for pos in positions.keys() {
-            let old_pos = positions.get(pos).unwrap();
-            let f = resultant_forces.get(pos).unwrap().scalar_mul(DELTA_T);
-            let new_pos = *old_pos + f;
-            new_positions.insert(*pos, new_pos);
-        }
-        positions = new_positions;
-        println!("=> {:?}", positions);
+        let repel_forces = calculate_repel_forces(g, &mut positions);
+        let spring_forces = calculate_spring_forces(g, &mut positions);
+        let resultant_forces = calculate_resultant_forces(repel_forces, spring_forces);
+        update_positions(&mut positions, resultant_forces);
     }
+    println!("=> {:?}", positions);
     positions
+}
+
+fn update_positions<ID: Debug + Copy + Ord + Clone + Hash + Eq>(
+    positions: &mut HashMap<ID, Vect>,
+    resultant_forces: HashMap<ID, Vect>,
+) -> HashMap<ID, Vect> {
+    let mut new_positions: HashMap<ID, Vect> = HashMap::new();
+    for pos in positions.keys() {
+        let old_pos = positions.get(pos).unwrap();
+        let f = resultant_forces.get(pos).unwrap().scalar_mul(DELTA_T);
+        let new_pos = *old_pos + f;
+        new_positions.insert(*pos, new_pos);
+    }
+    new_positions
+}
+
+fn calculate_resultant_forces<ID: Debug + Copy + Ord + Clone + Hash + Eq>(
+    repel_forces: HashMap<ID, Vect>,
+    spring_forces: HashMap<ID, Vect>,
+) -> HashMap<ID, Vect> {
+    let mut resultant_forces: HashMap<ID, Vect> = HashMap::new();
+    for u in repel_forces.keys() {
+        let temp_repel = repel_forces.get(u).unwrap();
+        let temp_spring = spring_forces.get(u).unwrap();
+        resultant_forces.insert(*u, *temp_repel + *temp_spring);
+    }
+    resultant_forces
+}
+
+fn calculate_spring_forces<T, E, ID: Debug + Copy + Ord + Clone + Hash + Eq>(
+    g: &Graph<T, E, ID>,
+    positions: &mut HashMap<ID, Vect>,
+) -> HashMap<ID, Vect> {
+    let mut spring_forces: HashMap<ID, Vect> = HashMap::new();
+    for node_u in g.nodes.keys().sorted() {
+        let mut spring_u = Vect::new(0., 0., 0.);
+        let u = positions.get(node_u).unwrap();
+        for node_v in g.neighbors(*node_u).unwrap() {
+            let v = positions.get(&node_v).unwrap();
+            spring_u = spring_u + spring_force(u, v) - repelling_force(u, v)
+        }
+        spring_forces.insert(*node_u, spring_u);
+    }
+    spring_forces
+}
+
+fn calculate_repel_forces<T, E, ID: Debug + Copy + Ord + Clone + Hash + Eq>(
+    g: &Graph<T, E, ID>,
+    positions: &mut HashMap<ID, Vect>,
+) -> HashMap<ID, Vect> {
+    //
+    let mut repel_forces: HashMap<ID, Vect> = HashMap::new();
+    for node_u in g.nodes.keys().sorted() {
+        let u = positions.get(node_u).unwrap();
+        let mut repel_u = Vect::new(0., 0., 0.);
+        for node_v in g.nodes.keys().sorted() {
+            if node_u != node_v {
+                let v = positions.get(node_v).unwrap();
+                repel_u = repel_u + repelling_force(u, v);
+            }
+        }
+        repel_forces.insert(*node_u, repel_u);
+    }
+    repel_forces
 }
 
 #[test]
